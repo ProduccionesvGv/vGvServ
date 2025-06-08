114fe1c1e2925e021dc741b22e990cecb33730af
ProduccionesvGv <gabrielvitalis34@gmail.com>
2025-06-08 02:28:26 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -3,23 +3,3 @@ document.getElementById('form-contacto').addEventListener('submit', function (e)
   alert("Mensaje enviado correctamente. Nos pondremos en contacto pronto.");
   this.reset();
 });
-
-
-document.querySelectorAll('.modal-trigger').forEach(img => {
-  img.addEventListener('click', () => {
-    document.getElementById('modal-title').textContent = img.dataset.title;
-    document.getElementById('modal-description').textContent = img.dataset.description;
-    document.getElementById('image-modal').style.display = 'block';
-  });
-});
-
-document.querySelector('.close-button').addEventListener('click', () => {
-  document.getElementById('image-modal').style.display = 'none';
-});
-
-window.addEventListener('click', (event) => {
-  const modal = document.getElementById('image-modal');
-  if (event.target === modal) {
-    modal.style.display = 'none';
-  }
-});

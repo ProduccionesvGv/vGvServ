8d1363746273d91cc0faccbde7551341cc26345b
ProduccionesvGv <gabrielvitalis34@gmail.com>
2026-01-28 03:02:24 -0300

    Delete js directory

--- js/fix-cards.js
+++
@@ -1,9 +0,0 @@
-// Unified renderer: delegate to data-driver's renderCards so #carousel usa el mismo template que #carousel2
-(function(){
-  function ready(fn){ if(document.readyState !== 'loading') fn(); else document.addEventListener('DOMContentLoaded', fn); }
-  ready(function(){
-    if (typeof renderCards === 'function') {
-      try { renderCards('carousel', (window.PRODUCTS||[])); } catch(e){}
-    }
-  });
-})();\n
